c54d64e9a4d86f250b77981265f7f1956ce93720
Dmitry Palamarchuk <moonbrv@gmail.com>
2016-12-07 01:16:52 +0200

    update npm commands

--- gulpfile.js
+++ gulpfile.js
@@ -25,8 +25,6 @@ var reload = bs.reload;
 var paths = {
 	style_src: "./assets/scss/**/*.scss",
 	style_dest: "./public/css/",
-	vendor_css: "./vendors/css/*.css",
-	vendor_js: "./vendors/js/*.js",
 	js_src: "./assets/js/index.js",
 	js_dest: "./public/js/",
 	html_src: "./assets/*.html",
@@ -41,6 +39,14 @@ var sassOptions = {
 	errLogToConsole: true,
 	outputStyle: 'expanded'
 };
+
+/* --- for_slick --- */
+gulp.task('for_slick', function(){
+	return gulp.src('node_modules/slick-carousel/slick/ajax-loader.gif')
+	.pipe(gulp.dest(paths.style_dest))
+	.pipe(gulp.src('node_modules/slick-carousel/slick/fonts/*'))
+	.pipe(gulp.dest('public/css/fonts'));
+});
 
 /* --- html --- */
 gulp.task('html', function(){
@@ -79,16 +85,6 @@ gulp.task('sass', function(){
 });
 
 /* --- javascripts --- */
-/*gulp.task('js', function(){
-	return gulp.src(paths.js_src)
-	.pipe(sourcemaps.init())
-	.pipe(concat('main.js'))
-	.pipe(uglify().on('error', gutil.log))
-	.pipe(rename({suffix: '.min'}))
-	.pipe(sourcemaps.write())
-	.pipe(gulp.dest(paths.js_dest));
-});*/
-
 const customOpts = {
 	entries: [paths.js_src],
 	debug: true
@@ -126,7 +122,7 @@ gulp.task('image', function () {
 });
 
 /* --- static server --- */
-gulp.task('serve', ['html', 'fonts', 'image', 'sass', 'javascript-watch'], function() {
+gulp.task('serve', ['html', 'for_slick', 'fonts', 'image', 'sass', 'javascript-watch'], function() {
 	bs.init([paths.style_dest, paths.js_dest, paths.html_dest, paths.img_dest],{
 		server: "./public/"
 	});
